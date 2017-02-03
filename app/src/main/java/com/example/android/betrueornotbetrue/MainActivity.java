bfd21e4c0248f3b32909f89d23caa10db927c755
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-02-02 18:59:25 -0600

    Initial Commit
        Code from the Android Programming by Big Nerd Rang book.
    
        Stopped at the end of chapter 1.

---
+++ app/src/main/java/com/example/android/betrueornotbetrue/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.betrueornotbetrue;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.correct_answer, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_answer, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
