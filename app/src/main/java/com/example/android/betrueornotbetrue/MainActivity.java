26ed6437b6c17a9db3fd9575d41579175e074de1
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-02-02 19:33:00 -0600

    Development
    Added a next Button (not wired up yet).  Also added an array of questions.
    
    Stopped on page 42.  Next step is to wire up the next button to display the next question and to add real question text to the questions.
    
    Changes to be committed:
        modified:   app/src/main/java/com/example/android/betrueornotbetrue/MainActivity.java
        modified:   app/src/main/res/layout/activity_main.xml
        modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/android/betrueornotbetrue/MainActivity.java
+++ app/src/main/java/com/example/android/betrueornotbetrue/MainActivity.java
@@ -4,17 +4,36 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+
+    private TextView mQuestionTextView;
+
+    // For more complex applicaitons this array would be created and stored elsewhere.
+    private Question[] mQuestionArray = new Question[] {
+        new Question(R.string.question1, true),
+        new Question(R.string.question2, true),
+        new Question(R.string.question3, true),
+        new Question(R.string.question4, true),
+        new Question(R.string.question5, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionArray[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -23,8 +42,17 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, R.string.correct_answer, Toast.LENGTH_SHORT).show();
             }
         });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_answer, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.false_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, R.string.incorrect_answer, Toast.LENGTH_SHORT).show();
