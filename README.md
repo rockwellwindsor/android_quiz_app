8ce279d98e9a51e0b974ddd561017c88930d7b52
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-02-05 17:33:27 -0600

    Fix
    Gitignore had incorrect content.  Perhaps it got dropped into the folder by mistake.
    
    footer

---
+++ README.md
@@ -0,0 +1,10 @@
+# To be or not to be true
+This is my version of the Geo Quiz application which is the first tutorial application for the book, <strong>Android Programming</strong> <i>The Big Nerd Ranch Guide</i>.
+
+It is a simple quiz application that present questions to the user and allows them to choose true or false.  The user may also elect to cheat if they do not know the answer and the answer will be revealed to them, however it will remind them they cheated when they go back and answer the question.
+
+## Installation
+
+````
+$ git clone https://github.com/rockwellwindsor/android_quiz_app.git
+````
---
+++ app/src/main/java/com/example/android/betrueornotbetrue/CheatActivity.java
@@ -0,0 +1,59 @@
+package com.example.android.betrueornotbetrue;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.betrueornotbetrue.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.betrueornotbetrue.answer_shown";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+}
